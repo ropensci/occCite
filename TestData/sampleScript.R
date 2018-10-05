58ee8dfdbcd7aaa1d98624febffa9af8867ab137
hannahlowens <hannah.owens@gmail.com>
2018-10-05 18:46:10 -0400

    Fixed up sample script.

--- TestData/sampleScript.R
+++ TestData/sampleScript.R
@@ -1,18 +1,18 @@
-library(BridgeTree);
+library(occCite);
 library(ape);
 
 #Get tree
-tree <- read.nexus("~/Dropbox/BridgeTree/TestData/Fish_12Tax_time_calibrated.tre");
+tree <- read.nexus("~/Dropbox/occCite/TestData/Fish_12Tax_time_calibrated.tre");
 # try
-tree <- read.nexus(system.file("extdata/Fish_12Tax_time_calibrated.tre",package='BridgeTree'))
+tree <- read.nexus(system.file("extdata/Fish_12Tax_time_calibrated.tre",package='occCite'))
 #Query databases for names
-myBridgeTreeObject <- studyTaxonList(x = tree, datasources = "NCBI");
+myOccCiteObject <- studyTaxonList(x = tree, datasources = "NCBI");
 
 #Query GBIF for occurrence data
 login <- GBIFLoginManager(user = "******",
                           email = "*****@*****",
                           pwd = "*****");
-myBridgeTreeObject <- occQuery(x = myBridgeTreeObject, GBIFLogin = login, GBIFDownloadDirectory = "~/Desktop");
+myOccCiteObject <- occQuery(x = myOccCiteObject, GBIFLogin = login, GBIFDownloadDirectory = "~/Desktop");
 
 #Get citations
-myOccCitations <- occCitation(myBridgeTreeObject);
+myOccCitations <- occCitation(myOccCiteObject);
