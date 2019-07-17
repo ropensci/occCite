085159854284e3d40b77bda44be2315bd01792ff
hannahlowens <hannah.owens@gmail.com>
2019-07-17 15:39:23 +0200

    Cleaning up vignette documentation.

--- inst/extdata/sampleScript.R
+++ inst/extdata/sampleScript.R
@@ -1,44 +1,88 @@
 library(occCite);
 library(ape);
 
-##Simple search
-#Query GBIF for occurrence data
-GBIFLogin <- GBIFLoginManager(user = "userName", email = "userName@awesome.pizza", pwd = "12345");
+# Simple search -----
+## Login information for GBIF ----
+GBIFLogin <- GBIFLoginManager(user = "userName",
+                              email = "userName@awesome.pizza",
+                              pwd = "12345");
 
 #From the beginning
-myOccCiteObject <- occQuery(x = "Protea cynaroides", GBIFLogin = GBIFLogin, datasources = c("gbif", "bien"), GBIFDownloadDirectory = "~/Desktop", loadLocalGBIFDownload = F, checkPreviousGBIFDownload = F);
-
-#Faster, if a download was previously prepared
-myOccCiteObject <- occQuery(x = "Protea cynaroides", GBIFLogin = GBIFLogin, GBIFDownloadDirectory = "~/Desktop", loadLocalGBIFDownload = F, checkPreviousGBIFDownload = T);
+myOccCiteObject <- occQuery(x = "Protea cynaroides",
+                            GBIFLogin = GBIFLogin,
+                            datasources = c("gbif", "bien"),
+                            GBIFDownloadDirectory = "~/Desktop",
+                            loadLocalGBIFDownload = F,
+                            checkPreviousGBIFDownload = F);
 
 #What does it look like?
 summary(myOccCiteObject)
 
 #Get citations
 myOccCitations <- occCitation(myOccCiteObject);
-cat(paste0(myOccCitations$Citation, " Accessed via ", myOccCitations$occSearch, " on ", myOccCitations$`Accession Date`, "."), sep = "\n");
 
-##Taxonomic rectification
-#Query databases for names and rectify
-myOccCiteObject <- studyTaxonList(x = "Protea cynaroides", datasources = c("FishBase", "EOL", "ARKive"));
+#Formatting a citation document
+cat(paste0(myOccCitations$Citation,
+           " Accessed via ", myOccCitations$occSearch,
+           " on ", myOccCitations$`Accession Date`, "."),
+    sep = "\n");
 
+#===============================================
+# Advanced features -----
+## Speeding up downloads ----
+# If a download was previously prepared
+myOccCiteObject <- occQuery(x = "Protea cynaroides",
+                            GBIFLogin = GBIFLogin,
+                            GBIFDownloadDirectory = "~/Desktop",
+                            loadLocalGBIFDownload = F,
+                            checkPreviousGBIFDownload = T);
 summary(myOccCiteObject)
 
-myOccCiteObject <- occQuery(x = myOccCiteObject, GBIFLogin = GBIFLogin, GBIFDownloadDirectory = "~/Desktop", loadLocalGBIFDownload = F, checkPreviousGBIFDownload = T);
+# If you have a download on your local machine
+myOccCiteObject <- occQuery(x = "Protea cynaroides",
+                            GBIFDownloadDirectory = "~/Desktop",
+                            loadLocalGBIFDownload = T,
+                            checkPreviousGBIFDownload = F);
+summary(myOccCiteObject)
 
-##With a tree
+#===============================================
+## Taxonomic rectification ------
+#Query databases for names and rectify
+taxRectObj <- studyTaxonList(x = "Protea cynaroides",
+                             datasources = c("FishBase", "EOL", "ARKive"));
+summary(taxRectObject)
+
+#Pass occCite object to query
+taxRectObject <- occQuery(x = taxRectObject,
+                          GBIFLogin = GBIFLogin,
+                          GBIFDownloadDirectory = "~/Desktop",
+                          loadLocalGBIFDownload = F,
+                          checkPreviousGBIFDownload = T);
+
+#===============================================
+# With a tree -----
 #Get tree
-tree <- read.nexus(system.file("extdata/Fish_12Tax_time_calibrated.tre", package='occCite'));
+tree <- read.nexus(system.file("extdata/Fish_12Tax_time_calibrated.tre",
+                               package='occCite'));
 plot(tree);
 
 #Query databases for names
-myOccCiteObject <- studyTaxonList(x = tree, datasources = "NCBI");
-
+myOccCiteObject <- studyTaxonList(x = tree,
+                                  datasources = "NCBI");
 summary(myOccCiteObject)
 
 #Query GBIF for occurrence data
-myOccCiteObject <- occQuery(x = myOccCiteObject, datasources = "gbif", GBIFLogin = GBIFLogin, GBIFDownloadDirectory = system.file("extdata/", package = 'occCite'), loadLocalGBIFDownload = F, checkPreviousGBIFDownload = T);
+myOccCiteObject <- occQuery(x = myOccCiteObject,
+                            datasources = "gbif",
+                            GBIFLogin = GBIFLogin,
+                            GBIFDownloadDirectory = system.file("extdata/",
+                                                                package = 'occCite'),
+                            loadLocalGBIFDownload = F,
+                            checkPreviousGBIFDownload = T);
 
 #Get citations
 myOccCitations <- occCitation(myOccCiteObject);
-cat(paste0(myOccCitations$Citation, " Accessed via ", myOccCitations$occSearch, " on ", myOccCitations$`Accession Date`, "."), sep = "\n");
+cat(paste0(myOccCitations$Citation,
+           " Accessed via ", myOccCitations$occSearch,
+           " on ", myOccCitations$`Accession Date`, "."),
+    sep = "\n");
