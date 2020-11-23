74d81ea06e11a40a5ff01b3c7f41f5f8b0dcc58f
hannahlowens <hannah.owens@gmail.com>
2020-11-23 15:53:33 +0100

    New tests for getGBIFpoints and tabGBIF

---
+++ tests/testthat/test-getGBIFpoints.R
@@ -0,0 +1,64 @@
+context("Getting GBIF points")
+
+library(occCite)
+
+test_that("GBIF retrieval from server behaves as expected", {
+  skip_on_cran()
+  cleanTaxon <- stringr::str_extract(string = "Protea cynaroides",
+                                     pattern = "(\\w+\\s\\w+)")
+  key <- rgbif::name_suggest(q=cleanTaxon, rank='species')$data$key[1]
+  occD <- prevGBIFdownload(key, GBIFLogin=GBIFLoginManager())
+  res <- rgbif::occ_download_get(key=occD, overwrite=TRUE,
+                                 file.path(system.file('extdata/',
+                                                       package='occCite')))
+
+  expect_equal(class(res), "occ_download_get")
+})
+
+test_that("new GBIF search behaves as expected", {
+  skip_on_cran()
+  GBIFLogin <- GBIFLoginManager()
+  cleanTaxon <- stringr::str_extract(string = "Protea cynaroides",
+                                     pattern = "(\\w+\\s\\w+)")
+  key <- rgbif::name_suggest(q=cleanTaxon, rank='species')$data$key[1]
+  occD <- rgbif::occ_download(rgbif::pred("taxonKey", value = key),
+                              rgbif::pred("hasCoordinate", TRUE), rgbif::pred("hasGeospatialIssue", FALSE),
+                              user = GBIFLogin@username, email = GBIFLogin@email,
+                              pwd = GBIFLogin@pwd)
+  while (rgbif::occ_download_meta(occD[1])$status != "SUCCEEDED"){
+    Sys.sleep(60)
+    print(paste("Still waiting for Protea cynaroides test download preparation to be completed. Time: ",
+                format(Sys.time(), format = "%H:%M:%S")))
+  }
+  res <- rgbif::occ_download_get(key=occD, overwrite=TRUE,
+                                 file.path(system.file('extdata/',
+                                                       package='occCite')))
+  expect_equal(class(res), "occ_download_get")
+})
+
+test_that("getGBIFpoints behaves as expected", {
+  skip_on_cran()
+  GBIFLogin <- GBIFLoginManager()
+  testResult <- getGBIFpoints(taxon="Protea cynaroides", GBIFLogin,
+                              file.path(system.file('extdata/', package='occCite')))
+  expect_equal(class(testResult), "list")
+  expect_equal(length(testResult), 3)
+
+  expect_true("OccurrenceTable" %in% names(testResult))
+  expect_true("Metadata" %in% names(testResult))
+  expect_true("RawOccurrences" %in% names(testResult))
+
+  expect_equal(class(testResult$OccurrenceTable), "data.frame")
+  expect_equal(class(testResult$Metadata), "occ_download_meta")
+  expect_equal(class(testResult$RawOccurrences), "occ_download_get")
+
+  expect_true("name" %in% colnames(testResult$OccurrenceTable))
+  expect_true("longitude" %in% colnames(testResult$OccurrenceTable))
+  expect_true("latitude" %in% colnames(testResult$OccurrenceTable))
+  expect_true("day" %in% colnames(testResult$OccurrenceTable))
+  expect_true("month" %in% colnames(testResult$OccurrenceTable))
+  expect_true("year" %in% colnames(testResult$OccurrenceTable))
+  expect_true("Dataset" %in% colnames(testResult$OccurrenceTable))
+  expect_true("DatasetKey" %in% colnames(testResult$OccurrenceTable))
+  expect_true("DataService" %in% colnames(testResult$OccurrenceTable))
+})
---
+++ tests/testthat/test-tabGBIF.R
@@ -0,0 +1,54 @@
+context("GBIF tabulation")
+
+library(occCite)
+
+test_that("data entered into tabGBIF is as expected", {
+  skip_on_cran()
+  key <- rgbif::name_suggest(q="Protea cynaroides", rank='species')$data$key[1]
+  GBIFLogin <- GBIFLoginManager()
+  occD <- prevGBIFdownload(key, GBIFLogin)
+  res <- rgbif::occ_download_get(key=occD, overwrite=TRUE,
+                                 file.path(system.file('extdata/', package='occCite')))
+
+  expect_equal(class(res), "occ_download_get")
+})
+
+test_that("verify occ_download_import results have expected columns", {
+  skip_on_cran()
+  key <- rgbif::name_suggest(q="Protea cynaroides", rank='species')$data$key
+  GBIFLogin <- GBIFLoginManager()
+  occD <- prevGBIFdownload(key, GBIFLogin)
+  res <- rgbif::occ_download_get(key=occD, overwrite=TRUE,
+                                 file.path(system.file('extdata/', package='occCite')))
+  occFromGBIF <- rgbif::occ_download_import(res)
+
+  expect_true("data.frame" %in% class(occFromGBIF))
+  expect_true("species" %in% colnames(occFromGBIF))
+  expect_true("decimalLongitude" %in% colnames(occFromGBIF))
+  expect_true("decimalLatitude" %in% colnames(occFromGBIF))
+  expect_true("day" %in% colnames(occFromGBIF))
+  expect_true("month" %in% colnames(occFromGBIF))
+  expect_true("year" %in% colnames(occFromGBIF))
+  expect_true("datasetName" %in% colnames(occFromGBIF))
+  expect_true("datasetKey" %in% colnames(occFromGBIF))
+})
+
+test_that("tabGBIF results as expected", {
+  key <- rgbif::name_suggest(q="Protea cynaroides", rank='species')$data$key
+  GBIFLogin <- GBIFLoginManager()
+  occD <- prevGBIFdownload(key, GBIFLogin)
+  res <- rgbif::occ_download_get(key=occD, overwrite=TRUE,
+                                 file.path(system.file('extdata/', package='occCite')))
+  occFromGBIF <- occCite:::tabGBIF(GBIFresults = res, "Protea cynaroides")
+
+  expect_equal(class(occFromGBIF), "data.frame")
+  expect_true("name" %in% colnames(occFromGBIF))
+  expect_true("longitude" %in% colnames(occFromGBIF))
+  expect_true("latitude" %in% colnames(occFromGBIF))
+  expect_true("day" %in% colnames(occFromGBIF))
+  expect_true("month" %in% colnames(occFromGBIF))
+  expect_true("year" %in% colnames(occFromGBIF))
+  expect_true("Dataset" %in% colnames(occFromGBIF))
+  expect_true("DatasetKey" %in% colnames(occFromGBIF))
+  expect_true("DataService" %in% colnames(occFromGBIF))
+})
