32c8731e79327360343a10aadd70ec4f4f8954f4
hannahlowens <hannah.owens@gmail.com>
2020-11-24 18:32:01 +0100

    Final batch of tests created.

---
+++ tests/testthat/test-studyTaxonList.R
@@ -0,0 +1,68 @@
+context("Verifies performance of studyTaxonList")
+
+library(occCite)
+
+test_that("phylogeny can be read and manipulated as expected", {
+  phylogeny <- ape::read.nexus(system.file("extdata/Fish_12Tax_time_calibrated.tre",
+                                           package = "occCite"))
+  phylogeny <- ape::extract.clade(phylogeny, 18)
+
+  expect_true(class(phylogeny) == "phylo")
+  expect_true(length(phylogeny$tip.label) == 7)
+})
+
+test_that("studyTaxonList works with a phylogeny", {
+  phylogeny <- ape::read.nexus(system.file("extdata/Fish_12Tax_time_calibrated.tre",
+                                           package = "occCite"))
+  phylogeny <- ape::extract.clade(phylogeny, 18)
+  testResult <- studyTaxonList(x = phylogeny, datasources = c('NCBI'))
+
+  expect_true(class(testResult) == "occCiteData")
+  expect_true("userQueryType" %in% slotNames(testResult))
+  expect_true("userSpecTaxonomy" %in% slotNames(testResult))
+  expect_true("cleanedTaxonomy" %in% slotNames(testResult))
+  expect_true("occSources" %in% slotNames(testResult))
+  expect_true("occCiteSearchDate" %in% slotNames(testResult))
+  expect_true("occResults" %in% slotNames(testResult))
+
+  expect_true(testResult@userQueryType == "User-supplied phylogeny.")
+  expect_true(testResult@userSpecTaxonomy == "NCBI")
+
+  expect_true(class(testResult@cleanedTaxonomy) == "data.frame")
+  expect_true(nrow(testResult@cleanedTaxonomy) == 7)
+  expect_true("Input Name" %in% colnames(testResult@cleanedTaxonomy))
+  expect_true("Best Match" %in% colnames(testResult@cleanedTaxonomy))
+  expect_true("Taxonomic Databases w/ Matches"
+              %in% colnames(testResult@cleanedTaxonomy))
+
+  expect_true(length(testResult@occSources) == 0)
+  expect_true(length(testResult@occCiteSearchDate) == 0)
+  expect_true(length(testResult@occResults) == 0)
+})
+
+test_that("studyTaxonList works with a vector of species", {
+  taxVector <- c("Buteo buteo", "Buteo buteo hartedi", "Buteo japonicus")
+  testResult <- studyTaxonList(x = taxVector, datasources = c('NCBI'))
+
+  expect_true(class(testResult) == "occCiteData")
+  expect_true("userQueryType" %in% slotNames(testResult))
+  expect_true("userSpecTaxonomy" %in% slotNames(testResult))
+  expect_true("cleanedTaxonomy" %in% slotNames(testResult))
+  expect_true("occSources" %in% slotNames(testResult))
+  expect_true("occCiteSearchDate" %in% slotNames(testResult))
+  expect_true("occResults" %in% slotNames(testResult))
+
+  expect_true(testResult@userQueryType == "User-supplied list of taxa.")
+  expect_true(testResult@userSpecTaxonomy == "NCBI")
+
+  expect_true(class(testResult@cleanedTaxonomy) == "data.frame")
+  expect_true(nrow(testResult@cleanedTaxonomy) == 3)
+  expect_true("Input Name" %in% colnames(testResult@cleanedTaxonomy))
+  expect_true("Best Match" %in% colnames(testResult@cleanedTaxonomy))
+  expect_true("Taxonomic Databases w/ Matches"
+              %in% colnames(testResult@cleanedTaxonomy))
+
+  expect_true(length(testResult@occSources) == 0)
+  expect_true(length(testResult@occCiteSearchDate) == 0)
+  expect_true(length(testResult@occResults) == 0)
+})
---
+++ tests/testthat/test-taxonRectification.R
@@ -0,0 +1,38 @@
+context("Verifies performance of taxonRectification")
+
+library(occCite)
+
+test_that("functions on which it depends function as necessary", {
+  sources <- taxize::gnr_datasources()
+
+  expect_true("data.frame" %in% class(sources))
+  expect_true("title" %in% colnames(sources))
+  expect_true("id" %in% colnames(sources))
+  expect_true("NCBI" %in% sources$title)
+
+  datasources <- "NCBI"
+  sourceIDs <- sources$id[sources$title %in% datasources]
+  temp <- taxize::gnr_resolve(sci = "Buteo buteo", data_source_ids = sourceIDs)
+
+  expect_true("data.frame" %in% class(temp))
+  expect_true("user_supplied_name" %in% colnames(temp))
+  expect_true("matched_name" %in% colnames(temp))
+  expect_true("data_source_title" %in% colnames(temp))
+  expect_true(nrow(temp) == 1)
+  expect_true(temp$user_supplied_name == temp$matched_name)
+  expect_true(temp$data_source_title == datasources)
+})
+
+test_that("taxonRectification performs as expected", {
+  testResult <- taxonRectification(taxName = "Buteo buteo hartedi",
+                                   datasources = 'NCBI')
+
+  expect_true(class(testResult) == "data.frame")
+  expect_true("Input Name" %in% colnames(testResult))
+  expect_true("Input Name" %in% colnames(testResult))
+  expect_true("Searched Taxonomic Databases w/ Matches" %in% colnames(testResult))
+  expect_true(nrow(testResult) == 1)
+  expect_true(testResult$`Input Name`[1] == "Buteo buteo hartedi")
+  expect_true(testResult$`Best Match`[1] == "Buteo buteo harterti")
+  expect_true(testResult$`Searched Taxonomic Databases w/ Matches` == "NCBI")
+})
