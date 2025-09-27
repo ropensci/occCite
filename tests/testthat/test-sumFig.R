28ed22ef580b67d54a726ad517c31d3dd82ed53a
hannahlowens <hannah.owens@gmail.com>
2025-09-27 15:09:56 +0200

    preventing breakage when waffle isn't available

--- tests/testthat/test-sumFig.R
+++ tests/testthat/test-sumFig.R
@@ -10,21 +10,25 @@ test_that("default sumFig settings work", {
   test <- plot(myOccCiteObject)
   expect_true("yearHistogram" %in% names(test))
   expect_true(is_ggplot(test[[1]]))
-  expect_true("source" %in% names(test))
-  expect_true(is_ggplot(test[[2]]))
-  expect_true("aggregator" %in% names(test))
-  expect_true(is_ggplot(test[[3]]))
+  if(requireNamespace("waffle")){
+    expect_true("source" %in% names(test))
+    expect_true(is_ggplot(test[[2]]))
+    expect_true("aggregator" %in% names(test))
+    expect_true(is_ggplot(test[[3]]))
+  }
 })
 
 test_that("sumFig works when plotting by species", {
   test <- plot(myOccCiteObject, bySpecies = T)
   expect_true(names(test) == "Protea cynaroides")
   expect_true("yearHistogram" %in% names(test[[1]]))
-  expect_true(is_ggplot(test[[1]][[1]]))
-  expect_true("source" %in% names(test[[1]]))
-  expect_true(is_ggplot(test[[1]][[2]]))
-  expect_true("aggregator" %in% names(test[[1]]))
-  expect_true(is_ggplot(test[[1]][[3]]))
+  if(requireNamespace("waffle")){
+    expect_true(is_ggplot(test[[1]][[1]]))
+    expect_true("source" %in% names(test[[1]]))
+    expect_true(is_ggplot(test[[1]][[2]]))
+    expect_true("aggregator" %in% names(test[[1]]))
+    expect_true(is_ggplot(test[[1]][[3]]))
+  }
 })
 
 test_that("sumFig works when plotting only year histogram by species", {
@@ -36,6 +40,7 @@ test_that("sumFig works when plotting only year histogram by species", {
 })
 
 test_that("sumFig works when plotting only source by species", {
+  skip_if(!requireNamespace("waffle"))
   data("myOccCiteObject")
   test <- plot(myOccCiteObject, bySpecies = T, plotTypes = "source")
   expect_true(names(test) == "Protea cynaroides")
@@ -44,6 +49,7 @@ test_that("sumFig works when plotting only source by species", {
 })
 
 test_that("sumFig works when plotting only aggregator by species", {
+  skip_if(!requireNamespace("waffle"))
   data("myOccCiteObject")
   test <- plot(x = myOccCiteObject, bySpecies = T, plotTypes = "aggregator")
   expect_true(names(test) == "Protea cynaroides")
